d630cad42de29a16475669028c403086345800a9
Yanis <56721402+Illusion-97@users.noreply.github.com>
2022-12-22 15:20:31 +0100

    server.js revert

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const jsonServer = require('json-server')
 const jsonServerAuth = require('json-server-auth')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-//const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults()
 
-server.db = router.db
-//server.use(middlewares)
+server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
